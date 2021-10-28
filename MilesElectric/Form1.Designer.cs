1f79181d0820a278ce8a4a189f256ed5726fa752
Ethan robards <yourtooslow@gmail.com>
2021-10-28 11:07:17 -0500

    first commit

---
+++ MilesElectric/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MilesElectric
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.memWatchTimer = new System.Windows.Forms.Timer(this.components);
+            this.attachBtn = new System.Windows.Forms.Button();
+            this.stgLabel = new System.Windows.Forms.Label();
+            this.respawnPosLabel = new System.Windows.Forms.Label();
+            this.savePosBtn = new System.Windows.Forms.Button();
+            this.posLabel = new System.Windows.Forms.Label();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.dimnesionCheck = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // memWatchTimer
+            // 
+            this.memWatchTimer.Interval = 10;
+            this.memWatchTimer.Tick += new System.EventHandler(this.memWatchTimer_Tick);
+            // 
+            // attachBtn
+            // 
+            this.attachBtn.Location = new System.Drawing.Point(8, 34);
+            this.attachBtn.Name = "attachBtn";
+            this.attachBtn.Size = new System.Drawing.Size(189, 48);
+            this.attachBtn.TabIndex = 0;
+            this.attachBtn.Text = "Attach";
+            this.attachBtn.UseVisualStyleBackColor = true;
+            this.attachBtn.Click += new System.EventHandler(this.attachBtn_Click);
+            // 
+            // stgLabel
+            // 
+            this.stgLabel.Location = new System.Drawing.Point(13, 140);
+            this.stgLabel.Name = "stgLabel";
+            this.stgLabel.Size = new System.Drawing.Size(184, 39);
+            this.stgLabel.TabIndex = 2;
+            this.stgLabel.Text = "Current Stage:";
+            // 
+            // respawnPosLabel
+            // 
+            this.respawnPosLabel.Location = new System.Drawing.Point(13, 179);
+            this.respawnPosLabel.Name = "respawnPosLabel";
+            this.respawnPosLabel.Size = new System.Drawing.Size(184, 43);
+            this.respawnPosLabel.TabIndex = 3;
+            this.respawnPosLabel.Text = "Current Respawn Position:";
+            // 
+            // savePosBtn
+            // 
+            this.savePosBtn.Enabled = false;
+            this.savePosBtn.Location = new System.Drawing.Point(8, 236);
+            this.savePosBtn.Name = "savePosBtn";
+            this.savePosBtn.Size = new System.Drawing.Size(184, 47);
+            this.savePosBtn.TabIndex = 4;
+            this.savePosBtn.Text = "Save Position";
+            this.savePosBtn.UseVisualStyleBackColor = true;
+            this.savePosBtn.Click += new System.EventHandler(this.savePosBtn_Click);
+            // 
+            // posLabel
+            // 
+            this.posLabel.Location = new System.Drawing.Point(13, 103);
+            this.posLabel.Name = "posLabel";
+            this.posLabel.Size = new System.Drawing.Size(184, 37);
+            this.posLabel.TabIndex = 5;
+            this.posLabel.Text = "Current Position:";
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Enabled = false;
+            this.refreshButton.Location = new System.Drawing.Point(13, 310);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(179, 29);
+            this.refreshButton.TabIndex = 6;
+            this.refreshButton.Text = "Refresh Pointers";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // dimnesionCheck
+            // 
+            this.dimnesionCheck.Location = new System.Drawing.Point(30, 371);
+            this.dimnesionCheck.Name = "dimnesionCheck";
+            this.dimnesionCheck.Size = new System.Drawing.Size(133, 22);
+            this.dimnesionCheck.TabIndex = 7;
+            this.dimnesionCheck.Text = "Save As 2D";
+            this.dimnesionCheck.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 399);
+            this.Controls.Add(this.dimnesionCheck);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.posLabel);
+            this.Controls.Add(this.savePosBtn);
+            this.Controls.Add(this.respawnPosLabel);
+            this.Controls.Add(this.stgLabel);
+            this.Controls.Add(this.attachBtn);
+            this.Name = "Form1";
+            this.Text = "Miles Electric";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.CheckBox dimnesionCheck;
+
+        private System.Windows.Forms.Button refreshButton;
+
+        private System.Windows.Forms.Label posLabel;
+
+        private System.Windows.Forms.Label stgLabel;
+        private System.Windows.Forms.Label respawnPosLabel;
+        private System.Windows.Forms.Button savePosBtn;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Timer memWatchTimer;
+        private System.Windows.Forms.Button attachBtn;
+
+        #endregion
+    }
+}
---
+++ MilesElectric/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Squalr.Engine.Memory;
+using Squalr.Engine.OS;
+
+namespace MilesElectric
+{
+    public partial class Form1 : Form
+    {
+        private ulong baseaddr;
+        private ulong basepos = 0x5BBE768;
+        private ulong baserestart = 0x52225C0;
+        private int[] offsetpos = new[] {8, 0x44};
+        private float[] currentpos = new float[3];
+        private float[] currentrestart = new float[3];
+        private string currentstage = "";
+        private ulong currentposaddr, currentrestartaddr;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void attachBtn_Click(object sender, EventArgs e)
+        {
+            
+            Process colorsProc = Process.GetProcessesByName("Sonic Colors - Ultimate").FirstOrDefault();
+            if (colorsProc == null)
+            {
+                MessageBox.Show("Could not attach");
+                return;
+            }
+
+            Processes.Default.OpenedProcess = colorsProc;
+            attachBtn.Enabled = false;
+            MessageBox.Show("Attached to colors successfully!");
+            baseaddr = (ulong)colorsProc.MainModule.BaseAddress.ToInt64();
+
+            currentposaddr = Reader.Default.Read<ulong>(baseaddr + basepos, out _);
+            currentposaddr = Reader.Default.Read<ulong>(currentposaddr + (ulong) offsetpos[0], out _);
+            currentposaddr = currentposaddr + (ulong) offsetpos[1];
+            currentrestartaddr = Reader.Default.Read<ulong>(baseaddr + baserestart, out _);
+            currentrestartaddr = Reader.Default.Read<ulong>(currentrestartaddr, out _);
+            currentrestartaddr = Reader.Default.Read<ulong>(currentrestartaddr + 0x170, out _);
+            currentrestartaddr += 0x40;
+            memWatchTimer.Enabled = true;
+            savePosBtn.Enabled = true;
+            refreshButton.Enabled = true;
+        }
+
+        private void memWatchTimer_Tick(object sender, EventArgs e)
+        {
+
+             
+            currentpos[0] = Reader.Default.Read<float>(currentposaddr, out _);
+            currentpos[1] = Reader.Default.Read<float>(currentposaddr + 4, out _);
+            currentpos[2] = Reader.Default.Read<float>(currentposaddr + 8, out _);
+
+            for (ulong i = 0; i < 3; i++)
+            {
+                currentrestart[i] = Reader.Default.Read<float>(currentrestartaddr + i * 4, out _);
+            }
+            
+            posLabel.Text = $"Current Position:\n({currentpos[0]},{currentpos[1]},{currentpos[2]})";
+            respawnPosLabel.Text = $"Current Respawn Position:\n({currentrestart[0]},{currentrestart[1]},{currentrestart[2]})";
+        }
+
+        private void savePosBtn_Click(object sender, EventArgs e)
+        {
+            if (memWatchTimer.Enabled)
+            {
+                for (ulong i = 0; i < 3; i++)
+                {
+                    Writer.Default.Write(currentrestartaddr + i * 4, currentpos[i]);
+                }
+
+                if (dimnesionCheck.Checked)
+                {
+                    Writer.Default.Write<byte>(currentrestartaddr + 0x10, 1);
+                    Writer.Default.Write<byte>(currentrestartaddr + 0x14, 9);
+                    
+                }
+                else
+                {
+                  Writer.Default.Write<byte>(currentrestartaddr + 0x10, 0);
+                  Writer.Default.Write<byte>(currentrestartaddr + 0x14, 0);
+                }
+                Writer.Default.Write(currentrestartaddr + 0x18, 0.0f);
+            }
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            currentposaddr = Reader.Default.Read<ulong>(baseaddr + basepos, out _);
+            currentposaddr = Reader.Default.Read<ulong>(currentposaddr + (ulong) offsetpos[0], out _);
+            currentposaddr = currentposaddr + (ulong) offsetpos[1];
+            currentrestartaddr = Reader.Default.Read<ulong>(baseaddr + baserestart, out _);
+            currentrestartaddr = Reader.Default.Read<ulong>(currentrestartaddr, out _);
+            currentrestartaddr = Reader.Default.Read<ulong>(currentrestartaddr + 0x170, out _);
+            currentrestartaddr += 0x40;
+        }
+    }
+}
